2b0f244bcdb0a7e46c425b609602c32f780b8676
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-09-28 19:58:12 -0700

    Initial Commit

---
+++ FloatingPointOps/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FloatingPointOps
+//
+//  Created by Gavin Gavington on 9/28/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
